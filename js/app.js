7e4dcedfd398d3381e15d357a99e34fa725ef60c
Yves Courtois <Courtois1337@gmail.com>
2015-10-14 01:58:59 -0500

    still working on the skeleton

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
-angular.module('bgca',['angular-loading-bar', 'ngAnimate', 'ngRoute'])
+angular.module('bgca',['angular-loading-bar', 'ngAnimate', 'ngRoute', 'nvd3'])
 .config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/about', {
-        templateUrl: 'views/about.html'
+    $routeProvider.when('/grade-results', {
+        templateUrl: 'views/grade_results.html'
     }).
-    when('/contact', {
-        templateUrl: 'views/contact.html'
+    when('/school-results', {
+        templateUrl: 'views/school_results.html'
     }).
     when('/', {
         templateUrl: 'views/main.html',
@@ -20,6 +20,63 @@ angular.module('bgca',['angular-loading-bar', 'ngAnimate', 'ngRoute'])
     $scope.selectedCharacter = "Fox";
     $scope.selectedOpponent  = "Falco";
     $scope.data.characters = ["Fox", "Falco", "Sheik", "Marth"];
+
+
+    $scope.chart_options = {
+      chart: {
+        type: 'pieChart',
+        height: 450,
+        donut: true,
+        x: function(d){return d.key;},
+        y: function(d){return d.y;},
+        showLabels: true,
+
+        pie: {
+          startAngle: function(d) { return d.startAngle/2 -Math.PI/2 },
+          endAngle: function(d) { return d.endAngle/2 -Math.PI/2 }
+        },
+        transitionDuration: 500,
+        legend: {
+          margin: {
+            top: 5,
+            right: 140,
+            bottom: 5,
+            left: 0
+          }
+        }
+      }
+    };
+
+    $scope.chart_data = [
+        {
+            key: "One",
+            y: 5
+        },
+        {
+            key: "Two",
+            y: 2
+        },
+        {
+            key: "Three",
+            y: 9
+        },
+        {
+            key: "Four",
+            y: 7
+        },
+        {
+            key: "Five",
+            y: 4
+        },
+        {
+            key: "Six",
+            y: 3
+        },
+        {
+            key: "Seven",
+            y: .5
+        }
+    ];
 
 
     $scope.number = 100;
