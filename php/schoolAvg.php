aedf91389f3447be6084fe9ab1edf537d2a412af
Yves Courtois <Courtois1337@gmail.com>
2015-10-14 23:50:17 -0500

    php returns json based on given parameters

--- php/schoolAvg.php
+++ php/schoolAvg.php
@@ -1,14 +1,18 @@
 <?
 
-$dbh = pg_connect("host=localhost dbname=BGCAREPORTING user=postgres");
-
-if (!$dbh) {
-    die("Error in connection: " . pg_last_error());
-}
+$dbh = pg_connect("host=localhost port=5433 dbname=postgres user=postgres password=aoeu.123") or die('Could not connect: ' . pg_last_error());
 
 $SCHOOL = $_GET["school"];
+$GRADE = $_GET["grade"];
+$sql = '';
 
-$sql = "SELECT AVG(TOTAL) FROM bgca_reporting_pre_survey WHERE SCHOOL = '$SCHOOL'";	
+if(!GRADE) {
+	$sql = "SELECT AVG(TOTAL) FROM bgca_reporting_pre_survey WHERE SCHOOL = '$SCHOOL'";
+}
+
+else {
+	$sql = "SELECT AVG(TOTAL) FROM bgca_reporting_pre_survey WHERE SCHOOL = '$SCHOOL' AND GRADE = '$GRADE'";	
+}
 
 $result = pg_query($dbh, $sql);
 
@@ -16,7 +20,9 @@ if (!$result) {
     die("Error in SQL query: " . pg_last_error());
 }
 
-$arr = array ('school'=>$result);
+$arr = array("school"=>$result);
+
+header('Content-Type: application/json');
 echo json_encode($arr);
 
 ?>
--- php/schoolData.php
+++ php/schoolData.php
@@ -1,26 +1,32 @@
 <?
 
-$dbh = pg_connect("host=localhost dbname=BGCAREPORTING user=postgres");
+$dbh = pg_connect("host=localhost port=5433 dbname=postgres user=postgres password=aoeu.123") or die('Could not connect: ' . pg_last_error());
 
 if (!$dbh) {
     die("Error in connection: " . pg_last_error());
 }
 
 $SCHOOL = $_GET["school"];
+$GRADE = $_GET["grade"];
 
-if(!SCHOOL) {
+$sql = '';
+
+if(!GRADE) {
 	$sql = "SELECT row_to_json(bgca_reporting_pre_survey) FROM bgca_reporting_pre_survey WHERE SCHOOL = '$SCHOOL'";	
-
-	$result = pg_query($dbh, $sql);
-
-	if (!$result) {
-	    die("Error in SQL query: " . pg_last_error());
-	}
-
-	$arr = array ('school'=>$result);
-	echo json_encode($arr);
-
+}
+else {
+	$sql = "SELECT row_to_json(bgca_reporting_pre_survey) FROM bgca_reporting_pre_survey WHERE SCHOOL = '$SCHOOL' AND GRADE = '$GRADE'";
 }
 
+$result = pg_query($dbh, $sql);
+
+if (!$result) {
+    die("Error in SQL query: " . pg_last_error());
+}
+
+$arr = array ('school'=>$result);
+
+header('Content-Type: application/json');
+echo json_encode($arr);
 
 ?>
