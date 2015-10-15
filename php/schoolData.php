37a0e78a60348bd83dc0a7fc2e1ea0e6b8d93764
unknown <courtois1337@gmail.com>
2015-10-14 21:27:57 -0500

    json school data

---
+++ php/schoolData.php
@@ -0,0 +1,26 @@
+<?
+
+$dbh = pg_connect("host=localhost dbname=BGCAREPORTING user=postgres");
+
+if (!$dbh) {
+    die("Error in connection: " . pg_last_error());
+}
+
+$SCHOOL = $_GET["school"];
+
+if(!SCHOOL) {
+	$sql = "SELECT row_to_json(bgca_reporting_pre_survey) FROM bgca_reporting_pre_survey WHERE SCHOOL = '$SCHOOL'";	
+
+	$result = pg_query($dbh, $sql);
+
+	if (!$result) {
+	    die("Error in SQL query: " . pg_last_error());
+	}
+
+	$arr = array ('school'=>$result);
+	echo json_encode($arr);
+
+}
+
+
+?>
