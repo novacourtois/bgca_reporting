2862f6b676a4b297510c9603f58f920df5fab44b
Yves Courtois <Courtois1337@gmail.com>
2015-10-18 11:46:13 -0500

    building out the data-fetching

--- php/schoolData.php
+++ php/schoolData.php
@@ -6,27 +6,58 @@ if (!$dbh) {
     die("Error in connection: " . pg_last_error());
 }
 
-$SCHOOL = $_GET["school"];
-$GRADE = $_GET["grade"];
 
 $sql = '';
 
-if(!GRADE) {
-	$sql = "SELECT row_to_json(bgca_reporting_pre_survey) FROM bgca_reporting_pre_survey WHERE SCHOOL = '$SCHOOL'";	
-}
-else {
-	$sql = "SELECT row_to_json(bgca_reporting_pre_survey) FROM bgca_reporting_pre_survey WHERE SCHOOL = '$SCHOOL' AND GRADE = '$GRADE'";
-}
+// array containing all the school's data
+$school_data = [];
 
-$result = pg_query($dbh, $sql);
+// grab each school
+$sql = "SELECT array(SELECT DISTINCT SCHOOL FROM bgca_reporting_pre_survey)";
+$SCHOOLS = pg_query($dbh, sql);
 
-if (!$result) {
-    die("Error in SQL query: " . pg_last_error());
+// build out each school's data
+foreach ($SCHOOLS as $school) {
+    $data = [];
+    $data["name"] = "$school";
+
+    // average
+    $sql = "SELECT AVG(TOTAL) FROM bgca_reporting_pre_survey where SCHOOL=\"$school\"";
+    $data["average"] = pg_query($dbh, $sql);
+
+    // num students
+    $sql = "SELECT COUNT(*) FROM bgca_reporting_pre_survey where SCHOOL=\"$school\"";
+    $data["num_students"] = pg_query($dbh, $sql);
+
+    // grades in school
+    $sql ="SELECT array(SELECT DISTINCT GRADES FROM bgca_reporting_pre_survey where SCHOOL)=\"$school\"";
+    $data["grades"] = [];
+
+    $grades = pg_query($dbh, $sql);
+
+    // build out grade data
+    foreach($grades as $grade) {
+    	$grade_data = [];
+    	$grade_data["grade"] = $grade;
+
+    	$sql = "SELECT AVG(TOTAL) FROM bgca_reporting_pre_survey where SCHOOL=\"$school\" and GRADE=\"$grade\"";
+	    $grade_data["average"] = pg_query($dbh, $sql);
+
+	    $sql = "SELECT COUNT(*) FROM bgca_reporting_pre_survey where SCHOOL=\"$school\" and GRADE=\"$grade\"";
+	    $grade_data["num_students"] = pg_query($dbh, $sql);
+
+	    // need to implement these two
+	    $grade_data["overall_results"] = [];
+	    $grade_data["results_per_question"] = [];
+
+
+    }
+
+
+    array_push($school_data, $data);
 }
-
-$arr = array ('school'=>$result);
 
 header('Content-Type: application/json');
-echo json_encode($arr);
+echo json_encode($school_data);
 
 ?>
