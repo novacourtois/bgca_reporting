7be3b79d26ef871369e339522e4d8d5fff06deb8
Yves Courtois <Courtois1337@gmail.com>
2015-10-21 20:07:54 -0500

    fixed some minor things

--- js/app.js
+++ js/app.js
@@ -43,29 +43,25 @@ angular.module('bgca',['angular-loading-bar', 'ngAnimate', 'ngRoute', 'nvd3'])
         redirectTo: '/'
     });
 }])
-.config(['cfpLoadingBarProvider', function(cfpLoadingBarProvider) {
-    cfpLoadingBarProvider.includeSpinner = true;
-    cfpLoadingBarProvider.includeBar = true;
-  }])
 .factory('gameTechGradeData', function($http) {
    return {
      pre_survey: function() {
-       return $http.get('sample_data/grade/grade_6.json').then(function(result) {
+       return $http.get('sample_data/grade/grades.json').then(function(result) {
            return result.data;
        });
      },
      post_survey: function() {
-       return $http.get('sample_data/grade/grade_6.json').then(function(result) {
+       return $http.get('sample_data/grade/grades.json').then(function(result) {
            return result.data;
        });
      },
      chart_options: function() {
-       return $http.get('sample_data/grade/grade_6.json').then(function(result) {
+       return $http.get('sample_data/grade/grades.json').then(function(result) {
            return result.data;
        });
      },
      chart_type: function() {
-       return $http.get('sample_data/grade/grade_6.json').then(function(result) {
+       return $http.get('sample_data/grade/grades.json').then(function(result) {
            return result.data;
        });
      }
@@ -74,22 +70,22 @@ angular.module('bgca',['angular-loading-bar', 'ngAnimate', 'ngRoute', 'nvd3'])
 .factory('gameTechSchoolData', function($http) {
    return {
      pre_survey: function() {
-       return $http.get('sample_data/school/grade_6.json').then(function(result) {
+       return $http.get('sample_data/school/schools.json').then(function(result) {
            return result.data;
        });
      },
      post_survey: function() {
-       return $http.get('sample_data/school/grade_6.json').then(function(result) {
+       return $http.get('sample_data/school/grades.json').then(function(result) {
            return result.data;
        });
      },
      chart_options: function() {
-       return $http.get('sample_data/school/grade_6.json').then(function(result) {
+       return $http.get('sample_data/school/grades.json').then(function(result) {
            return result.data;
        });
      },
      chart_type: function() {
-       return $http.get('sample_data/school/grade_6.json').then(function(result) {
+       return $http.get('sample_data/school/grades.json').then(function(result) {
            return result.data;
        });
      }
@@ -98,198 +94,39 @@ angular.module('bgca',['angular-loading-bar', 'ngAnimate', 'ngRoute', 'nvd3'])
 .controller('mainCtrl', function($scope, $http) {
     $scope.data = {};
 
-    $scope.chart_options = {
-      chart: {
-        type: 'pieChart',
-        height: 450,
-        donut: true,
-        x: function(d){return d.key;},
-        y: function(d){return d.y;},
-        showLabels: true,
-        transitionDuration: 500,
-        legend: {
-          margin: {
-            top: 5,
-            right: 140,
-            bottom: 5,
-            left: 0
-          }
-        }
-      }
-    };
+    $scope.chart_options = {};
 
-    $scope.chart_data = [
-        {
-            key: "One",
-            y: 5
-        },
-        {
-            key: "Two",
-            y: 2
-        },
-        {
-            key: "Three",
-            y: 9
-        },
-        {
-            key: "Four",
-            y: 7
-        },
-        {
-            key: "Five",
-            y: 4
-        },
-        {
-            key: "Six",
-            y: 3
-        },
-        {
-            key: "Seven",
-            y: .5
-        }
-    ];
+    $scope.chart_data = [];
 
 })
-.controller('gameTechPreSurveySchoolResultsCtrl', function($scope, $http) {
+.controller('gameTechPreSurveySchoolResultsCtrl', function($scope, $http, gameTechSchoolData) {
     $scope.data = {};
 
-    $scope.chart_options = {
-            chart: {
-                type: 'discreteBarChart',
-                height: 300,
-                margin : {
-                    top: 20,
-                    right: 20,
-                    bottom: 60,
-                    left: 55
-                },
-                x: function(d){return d.label;},
-                y: function(d){return d.value;},
-                showValues: true,
-                valueFormat: function(d){
-                    return d3.format(',.4f')(d);
-                },
-                transitionDuration: 500,
-                xAxis: {
-                    axisLabel: 'Grade'
-                },
-                yAxis: {
-                    axisLabel: 'Average',
-                    axisLabelDistance: 30
-                }
-            }
-        };
+    console.log('hello');
+    $scope.chart_options = {};
 
-    $scope.chart_data = [
-        {
-            key: "Cumulative Return",
-            values: [
-                {
-                    "label" : "6th" ,
-                    "value" : 1.1 / 6 * 100
-                } ,
-                {
-                    "label" : "7th" ,
-                    "value" : 1 / 6  * 100
-                } ,
-                {
-                    "label" : "8th" ,
-                    "value" : 2 / 6 * 100
-                } ,
-                {
-                    "label" : "School" ,
-                    "value" : 1.5 / 6 * 100
-                }
-            ]
-        }
-    ];
+    $scope.chart_data = [];
 
+    $scope.schools = {};
 
-    $scope.schools = {
-      'burnet':{},
-      'mendez': {}
-    };
-
-    $scope.get_school_data = function(school) {
-        
+    $scope.get_school_data = function() {        
         console.log('fetching info');
-        $http.get('sample_data/school/'+school+'_data.json')
-        .success(function (data, status) {
-            console.log('fetching info worked');
+        gameTechSchoolData.pre_survey().then(function(data) {
             console.log(data);
-            console.log(status);
-
-            $scope.schools[school] = data;
-            
-        })
-        .error(function (data, status){
-            console.log('fetching info failed');
+            $scope.schools = data;
         });
     };
 
-
-    angular.forEach($scope.schools, function(value, key) {
-        $scope.get_school_data(key);
-    });
+    $scope.get_school_data();
 })
 .controller('gameTechPreSurveyGradeResultsCtrl', function($scope, $http, gameTechGradeData) {
     $scope.data = {};
 
-    $scope.chart_options = {
-            chart: {
-                type: 'discreteBarChart',
-                height: 300,
-                margin : {
-                    top: 20,
-                    right: 20,
-                    bottom: 60,
-                    left: 55
-                },
-                x: function(d){return d.label;},
-                y: function(d){return d.value;},
-                showValues: true,
-                valueFormat: function(d){
-                    return d3.format(',.4f')(d);
-                },
-                transitionDuration: 500,
-                xAxis: {
-                    axisLabel: 'Grade'
-                },
-                yAxis: {
-                    axisLabel: 'Average',
-                    axisLabelDistance: 30
-                }
-            }
-        };
+    $scope.chart_options = {};
 
-    $scope.chart_data = [
-        {
-            key: "Cumulative Return",
-            values: [
-                {
-                    "label" : "6th" ,
-                    "value" : 1.1 / 6 * 100
-                } ,
-                {
-                    "label" : "7th" ,
-                    "value" : 1 / 6  * 100
-                } ,
-                {
-                    "label" : "8th" ,
-                    "value" : 2 / 6 * 100
-                } ,
-                {
-                    "label" : "School" ,
-                    "value" : 1.5 / 6 * 100
-                }
-            ]
-        }
-    ];
-
+    $scope.chart_data = [];
 
     $scope.grades = {};
-
-
 
     $scope.get_grade_data = function() {
         
